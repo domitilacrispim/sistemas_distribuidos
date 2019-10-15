ef9a9f2fc8956e09b2773827e84afc30cf92a33a
Arthur Borges <abrothguer@gmail.com>
2019-10-15 08:47:18 -0300

    Implementação teste do GRPC

--- src/client.py
+++ src/client.py
@@ -1,21 +1,42 @@
 from __future__ import print_function
 import logging
+import time
 
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
+import birdwiki_pb2
+import birdwiki_pb2_grpc
 
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+        stub = birdwiki_pb2_grpc.BirdWikiStub(channel)
+        while True:
+            time.sleep(3)
+            try:
+                # TESTE 0: LISTA DE AVES
+                response = stub.listBirds(birdwiki_pb2.BirdName(name='test0'))
+                print("RECEBI DO SERVER ESTAS AVES: ")
+                for bird in response:
+                    print("Bird: ", bird.name)
 
+                # TESTE 1: INFORMAÇÕES DE UMA AVE
+                response = stub.showBird(birdwiki_pb2.BirdName(name='test1'))
+                print("\nINFO DA AVE: ")
+                print(response.name)
+                print(response.text)
+
+                # TESTE 2: """"SALVANDO"""" UMA AVE
+                response = stub.saveBird(birdwiki_pb2.BirdPage(name="test2", text=""))
+                print("\nIS SAVED? ", response.saved)
+
+            except KeyboardInterrupt:
+                print("Goodbye")
+                channel.unsubscribe(close)
+                exit()
+
+def close(channel):
+    channel.close()
 
 if __name__ == '__main__':
     logging.basicConfig()
--- src/server.py
+++ src/server.py
@@ -3,19 +3,38 @@ import logging
 
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
+import birdwiki_pb2
+import birdwiki_pb2_grpc
 
+BIRDSONS = [
+    birdwiki_pb2.BirdInfo(name="Sabia", editing="false", editor=""),
+    birdwiki_pb2.BirdInfo(name="Pica-pau", editing="false", editor=""),
+    birdwiki_pb2.BirdInfo(name="Corvo", editing="false", editor="")
+]
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
+BIRDSON = birdwiki_pb2.BirdPage(name = "Corvo", text="Corvus is a widely distributed genus of medium-sized to large birds in the family Corvidae. The genus includes species commonly known as crows, ravens, rooks and jackdaws;")
 
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+CONF = birdwiki_pb2.Confirmation(saved = True)
+
+class BirdWikiServer(birdwiki_pb2_grpc.BirdWikiServicer):
+
+    def listBirds(self, request, context):
+        print("REQUEST IS", request)
+        for bird in BIRDSONS: # TODO: TROCAR BIRDSONS PARA DB
+            yield bird
+
+    def showBird(self, request, context):
+        print("REQUEST IS", request)
+        return BIRDSON
+
+    def saveBird(self, request, context):
+        print("REQUEST IS", request)
+        return CONF
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    birdwiki_pb2_grpc.add_BirdWikiServicer_to_server(BirdWikiServer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
